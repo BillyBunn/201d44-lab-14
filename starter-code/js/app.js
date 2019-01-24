e09646892e46a147be6f82b6bcfc885f9fbcc0d0
Billy Bunn <williammbunn@gmail.com>
2019-01-24 14:27:42 -0800

    Updates with progress on catalog.js

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -9,7 +9,7 @@ var Cart = function(items) {
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
   var item = new CartItem(product, quantity);
-  Cart.items.push(item); // might need to change 'Cart' to 'this'?
+  Cart.items.push(item);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
